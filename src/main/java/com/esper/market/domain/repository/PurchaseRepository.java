056ff29be3f92bb22026bf0795111dbfe9a88186
enzoesper <enzoesper7@gmail.com>
2022-12-17 17:32:15 -0300

    creating the Purchase domain

---
+++ src/main/java/com/esper/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.esper.market.domain.repository;
+
+import com.esper.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
